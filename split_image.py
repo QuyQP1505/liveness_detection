a95f9af2a8536f194cde505f8b515bbcae669725
unknown <you@example.com>
2022-11-12 12:16:59 +0700

    Upload split image function.

---
+++ split_image.py
@@ -0,0 +1,34 @@
+# Read image 
+
+import os 
+import cv2
+import shutil
+
+csv_path = r"D:\Github\Face-Liveness-Detection\data\lable_frames.csv"
+img_folder = r"D:\Github\Face-Liveness-Detection\data\frames"
+fake_img_folder = r"D:\Github\Face-Liveness-Detection\data\fake"
+real_img_folder = r"D:\Github\Face-Liveness-Detection\data\real"
+
+with open(csv_path, "r+") as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        split_line = line.split(",")
+        idx,filename,label = split_line
+        try:
+            if filename == 'Frame':
+                pass
+            else:
+                cur_image = os.path.join(img_folder, filename)
+                if label == str(0.0):
+                    shutil.move(
+                        cur_image, os.path.join(fake_img_folder, filename)
+                    )
+                else:
+                    shutil.move(
+                        cur_image, os.path.join(real_img_folder, filename)
+                )
+        except:
+            pass
+
+print("Done!")
